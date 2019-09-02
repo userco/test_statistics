cafc2ebbdf4687330c9d8cada5c32b080968ec4b
unknown <penelova.maria@gmail.com>
2019-09-02 10:05:47 -0700

    BG

--- app/Jobs/ImportFIle.php
+++ app/Jobs/ImportFIle.php
@@ -7,6 +7,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use DB;
 
 class ImportFIle implements ShouldQueue
 {
@@ -29,8 +30,21 @@ class ImportFIle implements ShouldQueue
      */
     public function handle()
     {
+		$i = 0;
+		//insert new test
+		$test = new Test;
         foreach($this->fileContents as $row){
-			
+			$j = 0;
+			foreach($row as $cell){
+				if($i==0 && $j> 2 && $cell){
+					//insert item key
+				}
+				if($i==1 && $j == 2 && $cell){
+					//insert count distractors
+				}
+				$j++;
+			}
+			$i++;	
 		}	
     }
 }
